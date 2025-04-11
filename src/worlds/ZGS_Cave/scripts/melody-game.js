102464442eb3c9d38117f05bda5e7f4076494dbd
Ray <rayrmrose@gmail.com>
2025-04-11 01:09:07 -0400

    removed context afs and added cave state persistence

--- src/worlds/ZGS_Cave/scripts/melody-game.js
+++ src/worlds/ZGS_Cave/scripts/melody-game.js
@@ -1,11 +1,6 @@
 // console.log("Melody init...");
 
 // TODO:
-// - Overlapping sounds
-// - diff sound if wrong
-// - Glowing animation on click
-// - Glowing animation + disable on completion
-// - Spawn seed in front of player on completion?
 // - Ambient sound
 
 const CONTEXT_AF = this;
@@ -16,94 +11,136 @@ const melody = ["B", "G", "Y", "R", "B", "G", "Y", "R", "Y", "G", "Y", "B"];
 
 let pointer = 0;
 let canClick = true;
+let instant = false;
 
-CONTEXT_AF.wallLight = scene.querySelector("#wall_light"); // mister wall light!
+wallLight = scene.querySelector("#wall_light"); // mister wall light!
 
 //get seed
-CONTEXT_AF.seed = scene.querySelector("#seed"); // mister seed!
-CONTEXT_AF.seedLight = scene.querySelector("#seed_light"); // mister seed light!
-// CONTEXT_AF.incorrectNote = scene.querySelector("#incorrect_music"); TODO
-CONTEXT_AF.melodyMusic = scene.querySelector("#melody_music"); // full melody music
+seed = scene.querySelector("#seed"); // mister seed!
+seedLight = scene.querySelector("#seed_light"); // mister seed light!
+// incorrectNote = scene.querySelector("#incorrect_music"); TODO
+melodyMusic = scene.querySelector("#melody_music"); // full melody music
 
-CONTEXT_AF.wrongNote = scene.querySelector("#incorrect_music"); // incorrect music
+wrongNote = scene.querySelector("#incorrect_music"); // incorrect music
 
 //get cube
-CONTEXT_AF.cube = scene.querySelector("#cube"); // green cube
-CONTEXT_AF.cubeLight = scene.querySelector("#cube_light"); // green cube light
-CONTEXT_AF.cubeNote = scene.querySelector("#cube_music"); // green cube note
+cube = scene.querySelector("#cube"); // green cube
+cubeLight = scene.querySelector("#cube_light"); // green cube light
+cubeNote = scene.querySelector("#cube_music"); // green cube note
 
 //get triangle
-CONTEXT_AF.triangle = scene.querySelector("#triangle"); // red triangle
-CONTEXT_AF.triangleLight = scene.querySelector("#triangle_light"); // red triangle light
-CONTEXT_AF.triangleNote = scene.querySelector("#triangle_music"); // red triangle note
+triangle = scene.querySelector("#triangle"); // red triangle
+triangleLight = scene.querySelector("#triangle_light"); // red triangle light
+triangleNote = scene.querySelector("#triangle_music"); // red triangle note
 
 //get diamond
-CONTEXT_AF.diamond = scene.querySelector("#diamond"); // blue diamond
-CONTEXT_AF.diamondLight = scene.querySelector("#diamond_light"); // blue diamond light
-CONTEXT_AF.diamondNote = scene.querySelector("#diamond_music"); // blue diamond note
-CONTEXT_AF.diamondNoteHigh = scene.querySelector("#diamond_music_high"); // blue diamond note (high)
+diamond = scene.querySelector("#diamond"); // blue diamond
+diamondLight = scene.querySelector("#diamond_light"); // blue diamond light
+diamondNote = scene.querySelector("#diamond_music"); // blue diamond note
+diamondNoteHigh = scene.querySelector("#diamond_music_high"); // blue diamond note (high)
 
 //get sphere
-CONTEXT_AF.sphere = scene.querySelector("#sphere"); // yellow sphere
-CONTEXT_AF.sphereLight = scene.querySelector("#sphere_light"); // yellow sphere light
-CONTEXT_AF.sphereNote = scene.querySelector("#sphere_music"); // yellow sphere note
+sphere = scene.querySelector("#sphere"); // yellow sphere
+sphereLight = scene.querySelector("#sphere_light"); // yellow sphere light
+sphereNote = scene.querySelector("#sphere_music"); // yellow sphere note
+
+checkCaveState();
+
+// melody function when completed
+function melodyComplete() {
+  // CONTEXT_AF = this;
+  console.log("Melody complete.");
+
+  if (!instant) {
+    setTimeout(function () {
+      melodyMusic.components.sound.playSound();
+    }, 1500);
+    setTimeout(function () {
+      seed.setAttribute("position", "0.3 2 0");
+      seed.addEventListener("click", function () {
+        seedLight.setAttribute("position", "0 -100 0");
+      });
+    }, 11000);
+  }
+  canClick = false;
+  cube.classList.remove("circles-interactive-object");
+  diamond.classList.remove("circles-interactive-object");
+  sphere.classList.remove("circles-interactive-object");
+  triangle.classList.remove("circles-interactive-object");
+
+  wallLight.setAttribute("position", "0 -100 0");
+  cubeLight.setAttribute("position", "0 -100 0");
+  diamondLight.setAttribute("position", "0 -100 0");
+  triangleLight.setAttribute("position", "0 -100 0");
+  sphereLight.setAttribute("position", "0 -100 0");
+
+  //set URL
+
+  const CONTEXT_AF = this;
+
+  //add this property to allow the seed to still be in hand when coming back
+  let url = new URL(window.location.href);
+  url.searchParams.set("caveState", "done");
+  history.replaceState(history.state, "", url.href);
+}
+
+function checkCaveState() {
+  getParams = function (url) {
+    var params = {};
+    var parser = document.createElement("a");
+    parser.href = url;
+    var query = parser.search.substring(1);
+    var vars = query.split("&");
+    for (var i = 0; i < vars.length; i++) {
+      var pair = vars[i].split("=");
+      params[pair[0]] = decodeURIComponent(pair[1]);
+    }
+    return params;
+  };
+  const params = getParams(window.location.href);
+
+  if (params.hasOwnProperty("caveState")) {
+    if (params["caveState"] === "done") {
+      //hard code this. dammit
+
+      instant = true;
+      melodyComplete();
+
+      console.log("Cave change is on!");
+    } else {
+      console.log("Cave change is off...");
+    }
+  }
+}
 
 function incorrectPress() {
   console.log("Wrong one!");
-  CONTEXT_AF.wrongNote.components.sound.stopSound();
-  CONTEXT_AF.wrongNote.components.sound.playSound();
+  wrongNote.components.sound.stopSound();
+  wrongNote.components.sound.playSound();
 
   //take away lights
-  CONTEXT_AF.cubeLight.setAttribute("position", "0 -100 0");
-  CONTEXT_AF.diamondLight.setAttribute("position", "0 -100 0");
-  CONTEXT_AF.triangleLight.setAttribute("position", "0 -100 0");
-  CONTEXT_AF.sphereLight.setAttribute("position", "0 -100 0");
+  cubeLight.setAttribute("position", "0 -100 0");
+  diamondLight.setAttribute("position", "0 -100 0");
+  triangleLight.setAttribute("position", "0 -100 0");
+  sphereLight.setAttribute("position", "0 -100 0");
 
   setTimeout(function () {
     console.log("da lights r back");
     //put back lights
-    CONTEXT_AF.cubeLight.setAttribute("position", "0 -0 0");
-    CONTEXT_AF.diamondLight.setAttribute("position", "0 -0 0");
-    CONTEXT_AF.triangleLight.setAttribute("position", "0 -0 0");
-    CONTEXT_AF.sphereLight.setAttribute("position", "0 -0 0");
+    cubeLight.setAttribute("position", "0 -0 0");
+    diamondLight.setAttribute("position", "0 -0 0");
+    triangleLight.setAttribute("position", "0 -0 0");
+    sphereLight.setAttribute("position", "0 -0 0");
   }, 1500);
 }
 
-// melody function when completed
-function melodyComplete() {
-  console.log("Melody complete.");
-
-  setTimeout(function () {
-    CONTEXT_AF.melodyMusic.components.sound.playSound();
-  }, 2000);
-  setTimeout(function () {
-    CONTEXT_AF.seed.setAttribute("position", "0.3 2 0");
-    CONTEXT_AF.seed.addEventListener("click", function () {
-      CONTEXT_AF.seedLight.setAttribute("position", "0 -100 0");
-    });
-  }, 11500);
-
-  canClick = false;
-  CONTEXT_AF.cube.classList.remove("circles-interactive-object");
-  CONTEXT_AF.diamond.classList.remove("circles-interactive-object");
-  CONTEXT_AF.sphere.classList.remove("circles-interactive-object");
-  CONTEXT_AF.triangle.classList.remove("circles-interactive-object");
-
-
-  CONTEXT_AF.wallLight.setAttribute("position", "0 -100 0");
-  CONTEXT_AF.cubeLight.setAttribute("position", "0 -100 0");
-  CONTEXT_AF.diamondLight.setAttribute("position", "0 -100 0");
-  CONTEXT_AF.triangleLight.setAttribute("position", "0 -100 0");
-  CONTEXT_AF.sphereLight.setAttribute("position", "0 -100 0");
-}
-
 // Event listeners for objects
-CONTEXT_AF.cube.addEventListener("click", function () {
+cube.addEventListener("click", function () {
   if (canClick) {
     if (melody[pointer] == "G") {
       pointer++;
-      CONTEXT_AF.cubeNote.components.sound.stopSound();
-      CONTEXT_AF.cubeNote.components.sound.playSound();
+      cubeNote.components.sound.stopSound();
+      cubeNote.components.sound.playSound();
     } else {
       pointer = 0;
       incorrectPress();
@@ -112,12 +149,12 @@ CONTEXT_AF.cube.addEventListener("click", function () {
   }
 });
 
-CONTEXT_AF.triangle.addEventListener("click", function () {
+triangle.addEventListener("click", function () {
   if (canClick) {
     if (melody[pointer] == "R") {
       pointer++;
-      CONTEXT_AF.triangleNote.components.sound.stopSound();
-      CONTEXT_AF.triangleNote.components.sound.playSound();
+      triangleNote.components.sound.stopSound();
+      triangleNote.components.sound.playSound();
     } else {
       pointer = 0;
       incorrectPress();
@@ -126,19 +163,19 @@ CONTEXT_AF.triangle.addEventListener("click", function () {
   }
 });
 
-CONTEXT_AF.diamond.addEventListener("click", function () {
+diamond.addEventListener("click", function () {
   if (canClick) {
     if (melody[pointer] == "B") {
       if (pointer >= 11) {
-        CONTEXT_AF.diamondNote.components.sound.stopSound();
-        CONTEXT_AF.diamondNote.components.sound.playSound();
+        diamondNote.components.sound.stopSound();
+        diamondNote.components.sound.playSound();
         setTimeout(() => {
           melodyComplete();
         }, "1000");
       } else {
         pointer++;
-        CONTEXT_AF.diamondNoteHigh.components.sound.stopSound();
-        CONTEXT_AF.diamondNoteHigh.components.sound.playSound();
+        diamondNoteHigh.components.sound.stopSound();
+        diamondNoteHigh.components.sound.playSound();
       }
     } else {
       pointer = 0;
@@ -148,12 +185,12 @@ CONTEXT_AF.diamond.addEventListener("click", function () {
   }
 });
 
-CONTEXT_AF.sphere.addEventListener("click", function () {
+sphere.addEventListener("click", function () {
   if (canClick) {
     if (melody[pointer] == "Y") {
       pointer++;
-      CONTEXT_AF.sphereNote.components.sound.stopSound();
-      CONTEXT_AF.sphereNote.components.sound.playSound();
+      sphereNote.components.sound.stopSound();
+      sphereNote.components.sound.playSound();
     } else {
       pointer = 0;
       incorrectPress();
